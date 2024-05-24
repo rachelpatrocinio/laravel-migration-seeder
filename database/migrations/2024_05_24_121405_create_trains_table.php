c24a11d67d228aa65fe5be43c0a659414b262226
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-24 15:42:40 +0200

    Added: migration trains

---
+++ database/migrations/2024_05_24_121405_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('company',150);
+            $table->string('departure_station',150);
+            $table->string('arrival_station',150);
+            $table->time('departure_time', precision: 0);
+            $table->time('arrival_time', precision: 0);
+            $table->integer('train_code');
+            $table->unsignedSmallInteger('number of carriages');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};

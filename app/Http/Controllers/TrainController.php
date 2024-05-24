6c3905da67d2ff29f54c0957367764d98311fd23
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-24 16:21:20 +0200

    Added: printed on screen all trains

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     public function home(){
-        return view('home');
+
+        $trains = Train::all();
+        return view('home', compact('trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,18 @@
 <div class="container">
     <div class="row">
         <h1>Trains</h1>
+        <ul>
+            @foreach ($trains as $train)
+            <li>
+                <p>{{ $train->company }}</p>
+                <p>{{ $train->departure_station }}</p>
+                <p>{{ $train->arrival_station }}</p>
+                <p>{{ $train->departure_time }}</p>
+                <p>{{ $train->arrival_time }}</p>
+                <p>{{ $train->train_code }}</p>
+            </li>
+            @endforeach
+        </ul>
     </div>
 </div>
 @endsection

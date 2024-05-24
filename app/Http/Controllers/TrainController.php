bbe095e414d1d6516a19fb023911109d347a322d
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-24 16:11:52 +0200

    Added: controller TrainController.php

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrainController extends Controller
+{
+    public function home(){
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [TrainController::class, 'home'])->name('home');

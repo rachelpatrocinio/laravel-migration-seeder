4bc4301235dc1a9204d0917998df0e9406b7076b
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-27 14:45:14 +0200

    Added: TrainSeeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for($i = 0; $i < 100; $i++){
+            $new_train = new Train();
+            
+
+        }
+    }
+}
